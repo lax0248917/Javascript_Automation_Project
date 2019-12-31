950abc68581ece1bde3d464043c1381b65594a66
lax0248917 <lax0248917@gmail.com>
2019-12-31 13:02:25 -0500

    Updates made

---
+++ linkedIn/app.js
@@ -0,0 +1,6 @@
+// App to run to follow all suggested hashtags in LinkedIn
+
+const followingBtns = document.querySelectorAll('.full-width');
+
+followingBtns.forEach(followbtn => followbtn.click());
+
