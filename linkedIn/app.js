ba2c77e5259e4996866c223565b9bbe7b583b974
lax0248917 <lax0248917@gmail.com>
2019-12-31 14:15:27 -0500

    Updates made

--- linkedIn/app.js
+++ linkedIn/app.js
@@ -1,6 +1,6 @@
 // App to run to follow all suggested hashtags in LinkedIn
 
-const followingBtns = document.querySelectorAll('.full-width');
+const followingBtns = document.querySelectorAll('.full-width'); // this selector selected all the buttons on the page, so it is not the best solution.
 
 followingBtns.forEach(followbtn => followbtn.click());
 
